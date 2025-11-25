93d76362d082f6c803167728b3ef8ba2a7d0b38b
Alex Stiller <alex.stiller@posteo.de>
2025-11-25 17:48:33 +0100

    * adding random Helper for more rnd controll

---
+++ Helper/MyRandom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AGL.Helper
+{
+    internal static class MyRandom
+    {
+        private static Random _random = new Random();
+        public static void Initialize(int seed) => _random = new Random(seed);
+        public static int Rand(int r) => _random.Next(r);
+        public static T Rand<T>(List<T> list) => list[_random.Next(list.Count)];
+
+        internal static float Range(int v1, int v2) => _random.Next(v1, v2);
+    }
+}
